8e69568bcec8dcac91731d28425024fe32382ad7
Yudha3 <aliffrianto2@gmail.com>
2021-03-15 22:51:21 +0700

    second

---
+++ Minggu2/tugas/person1.java
@@ -0,0 +1,20 @@
+
+package tugas;
+
+public class person1 {
+    public static void main(String[] args) {
+        
+        // initialize variable
+        String fName, lName, stuId, stuStatus;
+        fName = "Aliffrianto";
+        lName = "Yudha Prakoso";
+        stuId = "E41201726";
+        stuStatus = "Active";
+        
+        // show
+        System.out.println("Student Name\t: " + fName + " " + lName);
+        System.out.println("Student ID\t: " + stuId);
+        System.out.println("Student Status\t: " + stuStatus);
+        
+    }
+}
