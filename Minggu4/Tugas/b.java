1de0672b9e6ce972d030f27eed8959e34243d0ac
Yudha3 <aliffrianto2@gmail.com>
2021-04-04 22:55:08 +0700

    fourth

--- Minggu4/Tugas/b.java
+++ Minggu4/Tugas/b.java
@@ -5,15 +5,25 @@
 public class b {
     public static void main(String[] args){
 
-     System.out.println("            do_while");
-     System.out.println("Bilangan Kelipatan 2 (0-100)");
-     System.out.println("----------------------------");
-     System.out.println("----------------------------");
-
-     int kelipatan = 2;
+    System.out.println("          Do_While");
+    System.out.println("Bilangan Kelipatan 2 (0-100)");
+    System.out.println("============================");
+     
+    int kelipatan = 2;
         do {
             System.out.print(kelipatan + " ");
             kelipatan=kelipatan+2;
         } while(kelipatan <= 100);
+        
+    System.out.println(" ");
+        
+    System.out.println("Bilangan Perkalian 2 (0-100)");
+    System.out.println("============================");
+     
+    int perkalian = 2;
+        do {
+            System.out.print(perkalian + " ");
+            perkalian=perkalian*2;
+        } while(perkalian <= 100);
     }
 }
